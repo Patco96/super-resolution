e3d4514353a25550fd7bed34ec5e09d384e42349
Patco96 <patycobelli@gmail.com>
2022-02-20 18:29:32 -0300

    Adding comparisson to dashboard

--- dashboard.py
+++ dashboard.py
@@ -34,106 +34,119 @@ srresnet, srgan_generator, esrgan_generator = get_models()
 
 st.title("Super-Resolution")
 
+show_comparisson = st.checkbox("Show Comparisson", False)
 
-select_sample = False  # st.checkbox("Select a sample", False)
+if show_comparisson:
 
-results = []
+    folders = os.listdir(Path(__file__).parents[0] / "test/")
+    selected_folder = st.selectbox("Select a dataset", folders)
+    folder_path = Path(__file__).parents[0] / "test/" / selected_folder
+    images = [str(folder_path / image)
+              for image in os.listdir(folder_path) if not image.startswith(".")]
+    with st.expander("See images"):
+        st.image(images, width=100)
 
-if select_sample:
-    samples = os.listdir(Path(__file__).parents[0] / "images/samples/")
-
-    options = [f.split("/")[-1] for f in samples if not f.startswith(".")]
-
-    # Show samples
-    selected_image = st.selectbox("Select a sample", options,
-                                  format_func=format_func)
-    original_img = f"images/samples/{selected_image}"
 else:
-    original_img = st.file_uploader(
-        "Upload image", type=["jpg", "png", "jpeg"])
+    select_sample = st.checkbox("Select a sample", True)
 
-if original_img:
+    results = []
 
-    original_img = Image.open(original_img, mode="r")
+    if select_sample:
+        samples = os.listdir(Path(__file__).parents[0] / "test/samples/")
 
-    st.image(original_img, caption="Original image", use_column_width=True)
+        options = [f.split("/")[-1] for f in samples if not f.startswith(".")]
 
-    col1, col2 = st.columns(2)
+        # Show samples
+        selected_image = st.selectbox("Select a sample", options,
+                                      format_func=format_func)
+        original_img = f"test/samples/{selected_image}"
+    else:
+        original_img = st.file_uploader(
+            "Upload image", type=["jpg", "png", "jpeg"])
 
-    hr_img, lr_img = load_image(original_img)
-    with col1:
-        st.write("Nearest neighbour upsampling")
-        t0 = time.time()
-        nearest_img = lr_img.resize(
-            (hr_img.width, hr_img.height), Image.NEAREST)
-        t1 = time.time()
-        st.image(nearest_img, use_column_width=True)
-        results.append(
-            {"Model": "Nearest neighbour upsampling", "Image": nearest_img, "Time": t1-t0})
+    if original_img:
 
-        st.write("Bicubic upsampling")
-        t0 = time.time()
-        nearest_img = lr_img.resize(
-            (hr_img.width, hr_img.height), Image.NEAREST)
-        t1 = time.time()
-        st.image(nearest_img, use_column_width=True)
-        results.append({"Model": "Bicubic upsampling", "Image": nearest_img})
+        original_img = Image.open(original_img, mode="r")
 
-        st.write("SRResNet")
-        t0 = time.time()
-        srresnet_img = srresnet_predict(srresnet, lr_img)
-        t1 = time.time()
-        st.image(srresnet_img, use_column_width=True)
-        results.append(
-            {"Model": "SRResNet", "Image": srresnet_img, "Time": t1-t0})
+        st.image(original_img, caption="Original image", use_column_width=True)
 
-    with col2:
+        col1, col2 = st.columns(2)
 
-        st.write("SRGAN")
-        with st.spinner("Generating SRGAN image..."):
+        hr_img, lr_img = load_image(original_img)
+        with col1:
+            st.write("Nearest neighbour upsampling")
             t0 = time.time()
-            sr_img_srgan = srgan_predict(srgan_generator, lr_img)
+            nearest_img = lr_img.resize(
+                (hr_img.width, hr_img.height), Image.NEAREST)
             t1 = time.time()
-        st.image(sr_img_srgan, use_column_width=True)
-        results.append(
-            {"Model": "SRGAN", "Image": sr_img_srgan, "Time": t1-t0})
+            st.image(nearest_img, use_column_width=True)
+            results.append(
+                {"Model": "Nearest neighbour upsampling", "Image": nearest_img, "Time": t1-t0})
 
-        # st.write("SRGAN animals")
-        # with st.spinner("Generating SRGAN animals image..."):
-        #     sr_img_srgan = srgan_predict(srgan_generator_animals, lr_img)
-        # st.image(sr_img_srgan, use_column_width=True)
-
-        st.write("ESRGAN")
-        with st.spinner("Generating ESRGAN image..."):
+            st.write("Bicubic upsampling")
             t0 = time.time()
-            sr_img_esrgan = esrgan_predict(esrgan_generator, lr_img)
+            nearest_img = lr_img.resize(
+                (hr_img.width, hr_img.height), Image.NEAREST)
             t1 = time.time()
-        st.image(sr_img_esrgan, use_column_width=True)
-        results.append(
-            {"Model": "ESRGAN", "Image": sr_img_esrgan, "Time": t1-t0})
+            st.image(nearest_img, use_column_width=True)
+            results.append(
+                {"Model": "Bicubic upsampling", "Image": nearest_img})
 
-        st.write("High resolution")
-        st.image(hr_img, use_column_width=True)
-        results.append({"Model": "High resolution",
-                       "Image": hr_img, "Time": 0})
+            st.write("SRResNet")
+            t0 = time.time()
+            srresnet_img = srresnet_predict(srresnet, lr_img)
+            t1 = time.time()
+            st.image(srresnet_img, use_column_width=True)
+            results.append(
+                {"Model": "SRResNet", "Image": srresnet_img, "Time": t1-t0})
 
-    st.header("Metrics")
+        with col2:
 
-    def get_ssim(sr_img, hr_img):
-        return structural_similarity(
-            np.array(hr_img), np.array(sr_img), data_range=255.0
-        )
+            st.write("SRGAN")
+            with st.spinner("Generating SRGAN image..."):
+                t0 = time.time()
+                sr_img_srgan = srgan_predict(srgan_generator, lr_img)
+                t1 = time.time()
+            st.image(sr_img_srgan, use_column_width=True)
+            results.append(
+                {"Model": "SRGAN", "Image": sr_img_srgan, "Time": t1-t0})
 
-    np_hr_img = np.array(hr_img)
-    for idx, result in enumerate(results):
-        np_sr_img = np.array(result["Image"])
+            # st.write("SRGAN animals")
+            # with st.spinner("Generating SRGAN animals image..."):
+            #     sr_img_srgan = srgan_predict(srgan_generator_animals, lr_img)
+            # st.image(sr_img_srgan, use_column_width=True)
 
-        results[idx]["PSNR"] = peak_signal_noise_ratio(
-            np_hr_img, np_sr_img, data_range=255.0)
-        results[idx]["SSIM"] = structural_similarity(
-            np_hr_img, np_sr_img, data_range=255.0, multichannel=True)
+            st.write("ESRGAN")
+            with st.spinner("Generating ESRGAN image..."):
+                t0 = time.time()
+                sr_img_esrgan = esrgan_predict(esrgan_generator, lr_img)
+                t1 = time.time()
+            st.image(sr_img_esrgan, use_column_width=True)
+            results.append(
+                {"Model": "ESRGAN", "Image": sr_img_esrgan, "Time": t1-t0})
 
-    df = pd.DataFrame(results)
-    df.drop(columns=["Image"], inplace=True)
+            st.write("High resolution")
+            st.image(hr_img, use_column_width=True)
+            results.append({"Model": "High resolution",
+                            "Image": hr_img, "Time": 0})
 
-    st.table(df)
+        st.header("Metrics")
+
+        def get_ssim(sr_img, hr_img):
+            return structural_similarity(
+                np.array(hr_img), np.array(sr_img), data_range=255.0
+            )
+
+        np_hr_img = np.array(hr_img)
+        for idx, result in enumerate(results):
+            np_sr_img = np.array(result["Image"])
+
+            results[idx]["PSNR"] = peak_signal_noise_ratio(
+                np_hr_img, np_sr_img, data_range=255.0)
+            results[idx]["SSIM"] = structural_similarity(
+                np_hr_img, np_sr_img, data_range=255.0, multichannel=True)
+
+        df = pd.DataFrame(results)
+        df.drop(columns=["Image"], inplace=True)
+
+        st.table(df)
